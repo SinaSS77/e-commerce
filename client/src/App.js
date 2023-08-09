b28f495c0c13c70483c5b5916c24cfc042305d79
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-08-08 20:50:24 -0700

    Create pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () =>{
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>,
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>
+      }
+    ]
   },
-  {
-    path: "/products/:id",
-    element: <span>Category</span>,
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>,
-  },
+
 ]);
 
 function App() {
