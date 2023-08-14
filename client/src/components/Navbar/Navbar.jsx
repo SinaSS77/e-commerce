db336f63f978380d4b26247906e1e2cc3c654a8d
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-08-13 22:47:45 -0700

    Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,12 +4,24 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="wrapper">
       <div className='left'></div>
+      <div className="item">
+        <img src='/img/en.png' alt='' />
+        <KeyboardArrowDownIcon/>
+      </div>
+      <div className="item">
+        <span>USD</span>
+        <KeyboardArrowDownIcon/>
+      </div>
+      <div className="item">
+        <Link to="/products/1">Women</Link>
+      </div>
       <div className='center'></div>
       <div className='right'></div>
       </div>
