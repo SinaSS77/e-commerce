201ff72bf0fd58d396de313253a934f671e8e18f
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-08-20 22:09:09 -0700

    Footer $ Slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,47 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Products from "./pages/Products/Products";
-import Product from "./pages/Product/Product";
-import Navbar from "./components/Navbar/Navbar";
+import React from 'react';
+import { Children } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-import "./app.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./app.scss"
 
-const Layout = () =>{
+const Layout = () => {
   return (
     <div className="app">
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
-    children:[
+    element: <Layout />,
+    children: [
       {
-        path:"/",
-        element:<Home/>
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/products/:id",
-        element:<Products/>
+        path: "/products/:id",
+        element: <Products />,
       },
       {
-        path:"/product/:id",
-        element:<Product/>
-      }
-    ]
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
-
 ]);
 
 function App() {
   return (
-    <div >
+    <div>
       <RouterProvider router={router} />
     </div>
   );
